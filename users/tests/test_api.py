db1887f91c0c6ad96a5536b99144bf7df8989d08
Volodymyr Tkach <vovantkac5@gmail.com>
2023-10-02 10:19:33 +0300

    Fix lint problems

--- users/tests/test_api.py
+++ users/tests/test_api.py
@@ -6,7 +6,6 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 from django.contrib.auth import get_user_model
-from django.urls import reverse
 
 CREATE_USER_URL = '/api/users/'
 
