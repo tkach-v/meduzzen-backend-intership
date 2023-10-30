1ee1e8c9e1e04aca9c11baa8357eadc1cd86d234
Volodymyr Tkach <vovantkac5@gmail.com>
2023-10-30 12:48:57 +0200

    Add Enum to export_results

--- helpers/export_results.py
+++ helpers/export_results.py
@@ -1,4 +1,5 @@
 import csv
+from enum import Enum
 
 from django.http import HttpResponse
 from rest_framework.response import Response
@@ -6,10 +7,15 @@ from rest_framework.response import Response
 from quizz.serializers import ExportResultsSerializer
 
 
+class FileType(Enum):
+    CSV = 'csv'
+    JSON = 'json'
+
+
 def export_results(results, file_type):
     serializer = ExportResultsSerializer(results, many=True)
 
-    if file_type == "csv":
+    if file_type == FileType.CSV.value:
         response = HttpResponse(content_type='text/csv')
         fieldnames = ExportResultsSerializer.Meta.fields
 
@@ -17,8 +23,7 @@ def export_results(results, file_type):
         writer.writeheader()
         for row in serializer.data:
             writer.writerow(row)
-
-    else:
+    elif file_type == FileType.JSON.value:
         response = Response(serializer.data, content_type='application/json')
 
     response['Content-Disposition'] = f'attachment; filename="user_results.{file_type}"'
